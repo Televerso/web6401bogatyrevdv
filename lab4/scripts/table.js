fb61d30ea6e8d3b37ace2b09958a4b1d106061c1
Богатырев Дмитрий Владимирович <dbogat23@yandex.ru>
2025-03-26 20:02:54 +0400

    task 6 done

---
+++ lab4/scripts/table.js
@@ -0,0 +1,39 @@
+const tableDiv = document.getElementById("ccalTable");
+
+async function getData() {
+    request = new Request("http://localhost:8000/home",{
+        method: "POST"
+    });
+    try {
+        const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const jsonTable = await response.json();
+        console.log(`Response status: ${response.status}`);
+        return jsonTable.data
+
+    } 
+    catch (error) {
+        console.error(error.message);
+    }
+}
+
+
+async function loadTable() {
+    table = await getData();
+    for (let coffee of table) {
+        const item = document.createElement("li");
+        item.appendChild(document.createElement("span")).textContent = String(coffee.name + " - ");
+        item.appendChild(document.createElement("span")).textContent = String(coffee.cal + "ккал");
+        tableDiv.appendChild(item);
+    }
+    console.log("Table loaded");
+}
+
+window.addEventListener("load", function (event) {
+    console.log("Page is fully loaded");
+    console.log("Trying to fetch the table");
+    loadTable();
+});
